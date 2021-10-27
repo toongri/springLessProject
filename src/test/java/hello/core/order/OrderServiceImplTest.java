f6691fdbd3d99df331e7277b30768672448af79a
Toong_laptop <jhkim03284@gmail.com>
2021-10-27 18:08:03 +0900

    7th commit : 의존관계 자동주입 까지 완료

---
+++ src/test/java/hello/core/order/OrderServiceImplTest.java
@@ -0,0 +1,10 @@
+package hello.core.order;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.Or;
+
+public class OrderServiceImplTest {
+    @Test
+    void createOrder() {
+    }
+}
